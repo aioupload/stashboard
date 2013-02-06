72bcae37a7b5373bf8f2cd5d080982014de710ed
Michele Zonca <shatsar@gmail.com>
2013-02-06 12:17:49 -0800

    mashape settings

--- stashboard/settings.py
+++ stashboard/settings.py
@@ -2,10 +2,10 @@ import os
 
 DEBUG = False
 
-SITE_NAME = "Stashboard"
-SITE_AUTHOR = "Colonel Mustache"
-SITE_URL = "http://stashbooard.appspot.com"
-REPORT_URL = "mailto:help@stashboard.org"
+SITE_NAME = "Mashape Status"
+SITE_AUTHOR = "Mashaper"
+SITE_URL = "http://status.mashape.com"
+REPORT_URL = "mailto:support@mashape.org"
 
 # Twitter update settings
 TWITTER_CONSUMER_KEY = ''
